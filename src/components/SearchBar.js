a02793419f7ce8b3e90f9da5a71609f3990ac019
Chinmay <madeshichinmay@gmail.com>
2019-05-26 02:08:22 +0530

    Patched the Undefined error and sent data back to parent(App)

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,17 +8,22 @@ class SearchBar extends React.Component {
         //callback function called by the input tag whenever text changes
         console.log(event.target.value);
     }*/
-
-    onFormSubmit(event){
+    //arrow functions bind the function with the 'this' with the class instance
+    //another way to bind the this with class instance is to use a constructor
+    //and inside it use bind method...
+    onFormSubmit=(event)=>{
         event.preventDefault();//doesnt automatically submit when enter is pressed
+        
         //console.log(this.state.term);
+
+        this.props.on_User_Submit(this.state.term)
     
     }
 
 
     //onChange={this.onInputChange}
     render(){
-        console.log(this)
+        //console.log(this)
         return (
             <div className="ui segment ">
                 <form className="ui form" onSubmit={this.onFormSubmit}>
@@ -53,6 +58,17 @@ Input is told what its value is
 
 So basically controlled components are those components
 whose values are stored in the react world rather than HTMl word.
+
+
+sending the data back to parent component from child component::
+So basically we converted the App component into class component
+and then we declare a method into the class and pass it down as
+prop in this searchbar component 
+now we pass that state.term thing in this passed down prop(function) and
+this prop will act as an callback function in parent component
+
+
+
 
 
 
